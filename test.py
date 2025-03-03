a3e21a4d9b10844a2e15824548231c724a2554da
Sreecharan1234 <sreecharan9484@gmail.com>
2025-03-03 22:50:40 +0530

    Removed secret from files

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-from langchain_huggingface import HuggingFaceEndpoint
-sec_key="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"
-os.environ["HUGGINGFACEHUB_API_TOKEN"]=sec_key
-repo_id="mistralai/Mistral-7B-Instruct-v0.2"
-llm=HuggingFaceEndpoint(repo_id=repo_id,temperature=0.7,token=sec_key)
-print(llm.invoke(s))
